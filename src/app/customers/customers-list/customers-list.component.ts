e913ff80ae36ecda106be55aa3e710540223b353
leonardorleon <leonardorleon@gmail.com>
2019-12-03 18:12:12 +0100

    implemented the customers list. It comes from the parent customers.component.ts because in the customers.component.html the people array is passed into a customers input variable. This customers input variable is in customers-list.component.ts and is then stored to the filteredCustomers variable which is looped through to calculate the total.

--- src/app/customers/customers-list/customers-list.component.ts
+++ src/app/customers/customers-list/customers-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { ICustomer } from 'src/app/shared/interfaces';
 
@@ -7,15 +7,38 @@ import { ICustomer } from 'src/app/shared/interfaces';
   templateUrl: './customers-list.component.html',
 })
 export class CustomersListComponent implements OnInit {
-    
-    filteredCustomers: ICustomer[] = [];
-    customersOrderTotal: number;
-    currencyCode: string = 'USD';
-    
-    constructor() { }
-    
-    ngOnInit() {
+  private _customers: ICustomer[] = [];
+  
+  @Input() get customers(): ICustomer[] {
+    return this._customers;
+  }
 
+  set customers(value: ICustomer[]) {
+    if (value) {
+      this.filteredCustomers = this._customers = value;
+      this.calculateOrders();
     }
+  }
+
+  filteredCustomers: ICustomer[] = [];
+  customersOrderTotal: number;
+  currencyCode: string = 'USD';
+  
+  constructor() { }
+  
+  ngOnInit() {
+
+  }
+
+  calculateOrders() {
+    this.customersOrderTotal = 0;
+    this.filteredCustomers.forEach((cust: ICustomer) => {
+      this.customersOrderTotal += cust.orderTotal;
+    })
+  }
+
+  sort(prop: string) {
+    // A sorter service will handle the sorting
+  }
 
 }
