629fd8ae1970d9dce02bf589f06da84fb26c34a0
leonardorleon <leonardorleon@gmail.com>
2019-12-09 10:32:47 +0100

    injected DataService into the customers.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CustomersModule } from './customers/customers.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     CustomersModule,
+    CoreModule,
     SharedModule
   ],
   providers: [],
