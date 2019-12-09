5cc5908ef1327145a991cb8472cd0262cb505a88
leonardorleon <leonardorleon@gmail.com>
2019-12-09 12:39:03 +0100

    Just adding the comment that in the previous commit app.routing.module changed to app-routing.module to follow the convention on the tutorial. Same thing was done for customers-routing.module. Updated the imports and made sure it's not breaking the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { CustomersModule } from './customers/customers.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';  //originally was called app.routing.module so it was fixed to app-routing convention
 import { OrdersModule } from './orders/orders.module';
 
 @NgModule({
